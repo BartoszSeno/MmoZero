59545c631665b3c81edc076d9023b14863428327
ZeroSeno <bartoszymkowicz@gmail.com>
2023-11-04 19:38:34 +0100

    add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainPlace from "./MainGame";
 
 function App() {
   return (
     <>
-      <></>
+      <BrowserRouter basename="/MmoZero">
+        <Routes>
+          <Route path="/" element={<MainPlace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/MainGame/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+function MainPlace() {
+  return (
+    <>
+      <>aaa</>
+    </>
+  );
+}
+
+export default MainPlace;
